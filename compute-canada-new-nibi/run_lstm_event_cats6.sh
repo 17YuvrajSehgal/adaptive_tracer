b0e2d7700eb4332c979da32482ee9aa70f4d3f73
Yuvraj Sehgal <17yuvraj.sehgal@gmail.com>
2026-03-25 12:48:33 -0400

    final training updates time 12 hr

--- compute-canada-new-nibi/run_lstm_event_cats6.sh
+++ compute-canada-new-nibi/run_lstm_event_cats6.sh
@@ -6,7 +6,7 @@
 #SBATCH --cpus-per-task=12
 #SBATCH --gpus-per-node=h100:1
 #SBATCH --mem=32G
-#SBATCH --time=08:00:00
+#SBATCH --time=12:00:00
 #SBATCH --output=/scratch/yuvraj17/adaptive_tracing_scratch/adaptive_tracer/logs/%x-%j.out
 #SBATCH --mail-type=BEGIN,END,FAIL
 
