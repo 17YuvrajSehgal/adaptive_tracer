122963a12a5c90dd2294214ffaf46ee6e2b5dd0b
Yuvraj Sehgal <17yuvraj.sehgal@gmail.com>
2026-03-25 19:35:43 -0400

    reduced time to 8 hr

--- compute-canada-new-nibi/txt_dump_to_shards_cats5_seq4096.sh
+++ compute-canada-new-nibi/txt_dump_to_shards_cats5_seq4096.sh
@@ -14,7 +14,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=32G
-#SBATCH --time=12:00:00
+#SBATCH --time=08:00:00
 #SBATCH --output=/scratch/yuvraj17/adaptive_tracing_scratch/adaptive_tracer/logs/%x-%j.out
 #SBATCH --mail-type=BEGIN,END,FAIL
 
